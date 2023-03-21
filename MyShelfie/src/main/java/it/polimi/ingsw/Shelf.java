3fed209f22893cae9b64fb50ae7b9ff2f0a3c588
Irene_Lo_Presti <irene.lopresti@mail.polimi.it>
2023-03-21 15:34:15 +0100

    CLass Shelf implemented - not tested (Lo Presti Lussana)

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Shelf.java
@@ -0,0 +1,94 @@
+package it.polimi.ingsw;
+
+
+public class Shelf {
+    private Color[][] grid;
+    private int[][] beenThere;
+
+
+    public Shelf(){
+        this.grid = new Color[6][5];
+        //l'indice (0,0) è quello della prima cella in alto a sinistra
+        this.beenThere = new int[6][5];
+
+        for(int r=0; r<6; r++)
+            for(int c=0; c<5; c++){
+                grid[r][c] = Color.BLANK;
+                beenThere[r][c] = 0;
+            }
+    }
+
+    //OVERVIEW: inserimento della/e tessera/e selezionata/e (dentro il vettore littleHand)
+    // nella propria libreria nella colonna di indice columnIndex
+    public void insert(int columnIndex, Color[] littleHand){
+        //controllo se l'indice è corretto e la shelf non è piena prima di chiamare questa funz
+        for(int r=5; r>=0; r--){
+            if(grid[r][columnIndex]==Color.BLANK){
+                for(int i=0; i<littleHand.length; i++){ grid[r+i][columnIndex] = littleHand[i];}
+                break;
+            }
+        }
+    }
+
+    //OVERVIEW: controllo numero tessere dello stesso colore (color) adiacenti e restituisce il totale
+    private int spotCheck(){
+        int score=0;
+        int dimension;
+        for(int r=0; r<5; r++)
+            for(int c=0; c<6; c++){
+                if(beenThere[r][c]==0){
+                    dimension = spotDimention(grid[r][c],r,c,0);
+                    switch(dimension){
+                        case 1: break;
+                        case 2: break;
+                        case 3: score = score +2;
+                            break;
+                        case 4: score = score +3;
+                            break;
+                        case 5: score = score +5;
+                            break;
+                        default: score = score +8;
+                    }
+                }
+            }
+        return score;
+    }
+
+    //OVERVIEW: conta il numero di celle del colore color adiacenti a partire dalla cella
+    // di indice (startRow, startColumn)
+    private int spotDimention(Color color, int startRow, int startColumn, int counter){
+        beenThere[startRow][startColumn] = 1;
+        //right
+        if(startColumn+1<5 && beenThere[startRow][startColumn+1]!=1 && grid[startRow][startColumn+1].equals(color))
+            counter = 1 + spotDimention(color, startRow, startColumn+1, counter);
+
+        //left
+        if(startColumn-1>=0 && beenThere[startRow][startColumn-1]!=1 && grid[startRow][startColumn-1].equals(color))
+            counter = 1 + spotDimention(color, startRow, startColumn-1, counter);
+
+        //down
+        if(startRow+1<6 && beenThere[startRow+1][startColumn]!=1 && grid[startRow+1][startColumn].equals(color))
+            counter = 1 + spotDimention(color, startRow+1, startColumn, counter);
+
+        //up
+        if(startRow-1>=0 && beenThere[startRow-1][startColumn]!=1 && grid[startRow-1][startColumn].equals(color))
+            counter = 1 + spotDimention(color, startRow-1, startColumn, counter);
+
+        return counter;
+    }
+
+    //OVERVIEW: ritorna il numero di celle libere della colonna di indice columnIndex
+    private int columnFreeSpace(int columnIndex){
+        int r=0;
+        while(grid[r][columnIndex]==Color.BLANK && r<6)
+            r++;
+        return r;
+    }
+
+    //OVERVIEW: controlla se la libreria è piena
+    private boolean isShelfFull(){
+        for(int c=0; c<6; c++)
+            if(grid[0][c]==Color.BLANK) return false;
+        return true;
+    }
+}
