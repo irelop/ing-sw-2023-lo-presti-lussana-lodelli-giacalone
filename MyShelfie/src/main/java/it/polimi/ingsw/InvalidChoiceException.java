37a5515fe6e0d1bce56a189143272d9df951a68e
Andrea Giacalone <giacaloneandrea1@gmail.com>
2023-03-26 19:03:27 +0200

    Commit of InvalidChoiceException

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/InvalidChoiceException.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw;
+
+public class InvalidChoiceException extends Exception {
+    public InvalidChoiceException(boolean exCase) {
+        if (exCase) {
+            System.out.println("You have chosen an invalid cell");
+        } else {
+            System.out.println("You can't pick because there are still neighbours");
+        }
+    }
+}
