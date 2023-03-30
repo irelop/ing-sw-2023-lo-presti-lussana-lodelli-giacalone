8009bcbc12672898cfb5a783bad41fdbcdf3ae2f
Andrea Giacalone <andrea1.giacalone@mail.polimi.it>
2023-03-30 21:27:11 +0200

    Commit of O5UPatternStrategy

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/O5UPatternStrategy.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.PatternStrategy;
+
+import it.polimi.ingsw.StrategyInterface;
+import it.polimi.ingsw.Tile;
+
+import java.util.Arrays;
+
+public class O5UPatternStrategy implements StrategyInterface {
+
+    @Override
+    public boolean checkPattern(Tile[][] shelfSnapshot) {
+        int counter = 0;
+        int rows = shelfSnapshot.length;
+
+        for(int i = 0; i< rows;i++){
+            if(Arrays.stream(shelfSnapshot[i]).distinct().count()>=5) counter++;
+        }
+
+        if(counter >=2) return true;
+
+        return false;
+    }
+}
