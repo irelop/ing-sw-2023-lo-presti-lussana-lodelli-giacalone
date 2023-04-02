92ab9816148af961a7e9e8f8eec834056cefdafe
riccardo lodelli <riccardo.lodelli@mail.polimi.it>
2023-04-02 20:01:19 +0200

    commit of PersonalGoalCardTest

--- MyShelfie/src/main/java/it/polimi/ingsw/PersonalGoalDeck.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/PersonalGoalDeck.java
@@ -16,16 +16,20 @@ public class PersonalGoalDeck {
         personalGoalCardDeck = new ArrayList<>();
 
         ReadFileByLines reader = new ReadFileByLines();
-        reader.readFrom("PersonalGoalCards.txt");
+        reader.readFrom("src/txtfiles/PersonalGoalCards.txt");
 
         for (int i = 0; i < MAX_SIZE; i++) {
             personalGoalCardDeck.add(new PersonalGoalCard());
         }
     }
 
+    /**
+     * Draw a card
+     * @return a random chosen PersonalGoalCard form the 12 created
+     */
     public PersonalGoalCard drawPersonal() {
         Random random = new Random();
         int idx = random.nextInt(personalGoalCardDeck.size());
-        return personalGoalCardDeck.get(idx);
+        return personalGoalCardDeck.remove(idx);
     }
 }
