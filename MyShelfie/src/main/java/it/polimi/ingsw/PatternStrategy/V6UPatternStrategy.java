b99c888f2921143c09898ef674261c99f54d8c0a
Andrea Giacalone <andrea1.giacalone@mail.polimi.it>
2023-04-05 23:45:36 +0200

    Commit of V6UPatternStrategyTest
    Also fixed a problem with size of tmp array in the pattern strategy

--- MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/V6UPatternStrategy.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/V6UPatternStrategy.java
@@ -8,24 +8,25 @@ import java.util.Arrays;
 public class V6UPatternStrategy implements StrategyInterface {
     @Override
     public boolean checkPattern(Tile[][] shelfSnapshot) {
-        int counter = 0;
         int rows = shelfSnapshot.length;
         int columns = shelfSnapshot[0].length;
-        Tile[] tmp = new Tile[columns];
+        Tile[] tmp = new Tile[rows];
 
         /* I need to use a temporary array in order to save the chosen column, then I'll proceed to count the
            distinct elements
          */
-
-        for(int j=0;j<columns && counter < 2;j++){
+        int counter =0;
+        for(int j=0;j<columns;j++){
             for(int i=0;i<rows;i++){
                 tmp[i] = shelfSnapshot[i][j];
             }
 
-            if(Arrays.stream(tmp).filter(x->x!=Tile.BLANK && x!= Tile.NOT_VALID).distinct().count()==6) counter++;
+            if(Arrays.stream(tmp).filter(x->x!=Tile.BLANK && x!= Tile.NOT_VALID).distinct().count()==6) {
+                counter++;
+            }
         }
 
-        return(counter==2);
+        return counter==2;
 
     }
 }
