e87dd57fbdbbdad8b3f02148917205d567b84b13
Andrea Giacalone <andrea1.giacalone@mail.polimi.it>
2023-04-06 00:27:26 +0200

    Commit of Q2UPatternStrategyTest
    Also fixed a problem with the strategy: it did not check that the two groups should have been of the same type

--- MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/Q2UPatternStrategy.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/Q2UPatternStrategy.java
@@ -7,28 +7,38 @@ import it.polimi.ingsw.Tile;
 public class Q2UPatternStrategy implements StrategyInterface {
     @Override
     public boolean checkPattern(Tile[][] shelfSnapshot) {
-        int counter = 0;
         int rows = shelfSnapshot.length;
         int columns = shelfSnapshot[0].length;
+        Tile foundTile = Tile.BLANK;
 
-        for(int i=0;i<rows-1 && counter <2;i++){
-            for(int j=0;j<columns-1;j++){
-                if(shelfSnapshot[i][j] != Tile.BLANK &&
-                        shelfSnapshot[i][j] != Tile.NOT_VALID &&
-                        shelfSnapshot[i][j]==shelfSnapshot[i][j+1] &&
-                        shelfSnapshot[i][j]==shelfSnapshot[i+1][j] &&
-                        shelfSnapshot[i][j]==shelfSnapshot[i+1][j+1])
-                {
-                    counter++;
 
-                    shelfSnapshot[i][j]=Tile.NOT_VALID;
-                    shelfSnapshot[i][j+1]=Tile.NOT_VALID;
-                    shelfSnapshot[i+1][j]=Tile.NOT_VALID;
-                    shelfSnapshot[i+1][j+1]=Tile.NOT_VALID;
+        for(int i=0;i<rows-1;i++) {
+            for (int j = 0; j < columns - 1; j++) {
+                if (foundTile == Tile.BLANK) {
+                    if (shelfSnapshot[i][j] != Tile.BLANK && shelfSnapshot[i][j] != Tile.NOT_VALID) {
+                        if (shelfSnapshot[i][j] == shelfSnapshot[i][j + 1] &&
+                                shelfSnapshot[i][j] == shelfSnapshot[i + 1][j] &&
+                                shelfSnapshot[i][j] == shelfSnapshot[i + 1][j + 1]) {
+                            foundTile = shelfSnapshot[i][j];
+                            shelfSnapshot[i][j] = Tile.NOT_VALID;
+                            shelfSnapshot[i][j + 1] = Tile.NOT_VALID;
+                            shelfSnapshot[i + 1][j] = Tile.NOT_VALID;
+                            shelfSnapshot[i + 1][j + 1] = Tile.NOT_VALID;
+                        }
+                    }
+                } else if(foundTile!=Tile.BLANK && foundTile!=Tile.NOT_VALID){
+                    if (shelfSnapshot[i][j] != Tile.BLANK && shelfSnapshot[i][j] != Tile.NOT_VALID) {
+                        if (shelfSnapshot[i][j] == foundTile &&
+                                shelfSnapshot[i][j] == shelfSnapshot[i][j + 1] &&
+                                shelfSnapshot[i][j] == shelfSnapshot[i + 1][j] &&
+                                shelfSnapshot[i][j] == shelfSnapshot[i + 1][j + 1]) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
-
-        return (counter==2);
+        return false;
     }
+
 }
