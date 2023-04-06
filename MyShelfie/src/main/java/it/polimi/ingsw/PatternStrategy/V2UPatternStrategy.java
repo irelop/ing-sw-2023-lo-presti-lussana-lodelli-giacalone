db799cf7450c70348c83dde1a0a3aefc472df1db
Andrea Giacalone <andrea1.giacalone@mail.polimi.it>
2023-04-06 23:17:04 +0200

    Fixed Problem in V2UPatternStrategy
    Different algorithm to check all possible rows and columns without exit from matrix bounds

--- MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/V2UPatternStrategy.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/V2UPatternStrategy.java
@@ -7,28 +7,26 @@ public class V2UPatternStrategy implements StrategyInterface {
     @Override
     public boolean checkPattern(Tile[][] shelfSnapshot) {
         int counter = 0;
-
         int rows = shelfSnapshot.length;
         int columns = shelfSnapshot[0].length;
 
-        for(int i = 0; i<rows-1; i++){
-            for(int j = 0; j< columns -1; j++){
-
-                //checking the adjacent cell on the right
-                if(shelfSnapshot[i][j] != Tile.BLANK && shelfSnapshot[i][j] != Tile.NOT_VALID) {
-                    if (shelfSnapshot[i][j] == shelfSnapshot[i][j + 1]) {
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                if(shelfSnapshot[i][j] != Tile.BLANK && shelfSnapshot[i][j] != Tile.NOT_VALID){
+                    if((i+1) < rows && shelfSnapshot[i][j] == shelfSnapshot[i+1][j]){
                         counter++;
                         shelfSnapshot[i][j] = Tile.NOT_VALID;
-                        shelfSnapshot[i][j + 1] = Tile.NOT_VALID;
-                    } else if (shelfSnapshot[i][j] == shelfSnapshot[i + 1][j]) {
+                        shelfSnapshot[i+1][j] = Tile.NOT_VALID;
+                    }
+                    else if((j+1) < columns && shelfSnapshot[i][j] == shelfSnapshot[i][j+1]){
                         counter++;
                         shelfSnapshot[i][j] = Tile.NOT_VALID;
-                        shelfSnapshot[i + 1][j] = Tile.NOT_VALID;
+                        shelfSnapshot[i][j+1] = Tile.NOT_VALID;
                     }
                 }
             }
         }
 
-        return (counter==6);
+        return (counter == 6);
     }
 }
