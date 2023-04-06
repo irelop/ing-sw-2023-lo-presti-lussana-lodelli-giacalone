c646cbea42c9b8ea5e9fd61f4abf2b997d4a7b5f
Andrea Giacalone <andrea1.giacalone@mail.polimi.it>
2023-04-06 14:27:32 +0200

    Change algorithm in PPatternStrategy
    Functional approach which now allows to dectect the false case and count properly the lenght of each column

--- MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/PPatternStrategy.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/PatternStrategy/PPatternStrategy.java
@@ -3,10 +3,70 @@ package it.polimi.ingsw.PatternStrategy;
 import it.polimi.ingsw.StrategyInterface;
 import it.polimi.ingsw.Tile;
 
+import java.util.Arrays;
+
 /**
  * a class implementing the P pattern strategy of the corresponding common goal card.
  */
 public class PPatternStrategy implements StrategyInterface {
+    @Override
+    public boolean checkPattern(Tile[][] shelfSnapshot) {
+        int rows = shelfSnapshot.length;
+        int columns = shelfSnapshot[0].length;
+        Tile[] firstColumn = new Tile[rows];
+        Tile[] decisionColumn = new Tile[rows]; //the second column: it indicates the ascending or descending order
+
+        for (int i = 0; i < rows; i++) {
+            firstColumn[i] = shelfSnapshot[i][0];
+        }
+
+        for (int i = 0; i < rows; i++) {
+            decisionColumn[i] = shelfSnapshot[i][1];
+        }
+        int numTilesColumn = Math.toIntExact(Arrays.stream(decisionColumn).filter(x -> x != Tile.BLANK && x != Tile.NOT_VALID).count());
+
+        //checking ascending branch
+        if (numTilesColumn ==
+                Arrays.stream(firstColumn).filter(x -> x != Tile.BLANK && x != Tile.NOT_VALID).count() + 1) {
+            for (int j = 2; j < columns; j++) {
+                Tile[] tmp = new Tile[rows];
+                for (int i = 0; i < rows; i++) {
+                    tmp[i] = shelfSnapshot[i][j];
+                }
+                if ((Arrays.stream(tmp).filter(x -> x != Tile.BLANK && x != Tile.NOT_VALID).count()) !=
+                        numTilesColumn+ 1) {
+                    System.out.println(Arrays.stream(tmp).filter(x -> x != Tile.BLANK && x != Tile.NOT_VALID).count());
+                    return false;
+                }
+                numTilesColumn = Math.toIntExact(Arrays.stream(tmp).filter(x -> x != Tile.BLANK && x != Tile.NOT_VALID).count());
+            }
+            return true;
+        }
+
+
+        //checking descending branch
+        else if (numTilesColumn ==
+                Arrays.stream(firstColumn).filter(x -> x != Tile.BLANK && x != Tile.NOT_VALID).count() - 1) {
+            for (int j = 2; j < columns; j++) {
+                Tile[] tmp = new Tile[rows];
+                for (int i = 0; i < rows; i++) {
+                    tmp[i] = shelfSnapshot[i][j];
+                }
+                if ((Arrays.stream(tmp).filter(x -> x != Tile.BLANK && x != Tile.NOT_VALID).count()) !=
+                        numTilesColumn - 1){
+                    return false;
+                }
+                numTilesColumn = Math.toIntExact(Arrays.stream(tmp).filter(x -> x != Tile.BLANK && x != Tile.NOT_VALID).count());
+            }
+            return true;
+        }
+
+        else return false;
+    }
+}
+    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    /*ATTENTION: NOT WORKING - DOESN'T CHECK THE FALSE CASE
     @Override
     public boolean checkPattern(Tile[][] shelfSnapshot) {
         int numCol = shelfSnapshot[0].length;
@@ -29,6 +89,4 @@ public class PPatternStrategy implements StrategyInterface {
             }
             return true;
         }
-        return false;
-    }
-}
+        return false;*/
