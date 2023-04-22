46f68902e2b5ea6a73a44959f2d88889f646118a
Irene_Lo_Presti <irene.lopresti@mail.polimi.it>
2023-04-22 13:49:22 +0200

    Javadoc of ChooseInitialPositionView

--- MyShelfie/src/main/java/it/polimi/ingsw/Client/View/ChooseInitialPositionView.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Client/View/ChooseInitialPositionView.java
@@ -1,9 +1,13 @@
 package it.polimi.ingsw.Client.View;
-
 import it.polimi.ingsw.Model.Exceptions.OutOfBoardException;
 import it.polimi.ingsw.Server.Messages.*;
-
 import java.util.Scanner;
+
+/**
+ * View where the client chooses the initial position of the tiles that he/she wants to choose from the board
+ * @author Irene Lo Presti
+ */
+
 
 public class ChooseInitialPositionView extends View{
 
@@ -12,12 +16,21 @@ public class ChooseInitialPositionView extends View{
     private PlayerNicknameMsg playerPlayingNicknameMsg;
     private MaxTilesPickableMsg maxTilesPickableMsg;
 
+    /**
+     * OVERVIEW: constructor method
+     * @param playerPlayingNicknameMsg: message from the server with the nickname of the player
+     * @param maxTilesPickableMsg: message from the server with the maximum number of tiles pickable
+     *                           from the board
+     */
     public ChooseInitialPositionView(PlayerNicknameMsg playerPlayingNicknameMsg, MaxTilesPickableMsg maxTilesPickableMsg){
 
         this.playerPlayingNicknameMsg = playerPlayingNicknameMsg;
         this.maxTilesPickableMsg = maxTilesPickableMsg;
     }
 
+    /**
+     * OVERVIEW: in this method the player chooses the initial position
+     */
     @Override
     public void run(){
 
@@ -59,21 +72,31 @@ public class ChooseInitialPositionView extends View{
             getOwner().transitionToView(nextView);
     }
 
+    /**
+     * OVERVIEW: getter method for the initial row
+     * @return row>=1 && row<MAX_ROWS
+     * @throws OutOfBoardException if the row is not between 1 and MAX_ROWS
+     */
     private int getInitialRow() throws OutOfBoardException {
         int r;
         System.out.print("Please insert the row of the initial position: ");
         Scanner scanner = new Scanner(System.in);
         r = scanner.nextInt();
-        if(r<0 || r>=MAX_COLUMNS) throw new OutOfBoardException();
+        if(r<=0 || r>MAX_ROWS) throw new OutOfBoardException();
         return r;
     }
 
+    /**
+     * OVERVIEW: getter method for the initial column
+     * @return column>=1 && column<MAX_ROW
+     * @throws OutOfBoardException if the column is not between 1 and MAX_COLUMNS
+     */
     private int getInitialColumn() throws OutOfBoardException{
         int c;
         System.out.print("Please insert the column of the initial position: ");
         Scanner scanner = new Scanner(System.in);
         c = scanner.nextInt();
-        if(c<0 || c>=MAX_COLUMNS) throw new OutOfBoardException();
+        if(c<=0 || c>MAX_COLUMNS) throw new OutOfBoardException();
         else return c;
     }
 }
