6499c74bcdfae0ad5b1e3b077ec562c08ff3490a
matteo-lussana <matteo.lussana@mail.polimi.it>
2023-04-22 11:59:22 +0200

    First draft of Client class and ServerHandler class

--- MyShelfie/src/main/java/it/polimi/ingsw/Client/Client.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Client/Client.java
@@ -1,4 +1,83 @@
 package it.polimi.ingsw.Client;
 
-public class Client {
+import it.polimi.ingsw.Client.View.ChooseInitialPositionView;
+import it.polimi.ingsw.Client.View.View;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client implements Runnable{
+    private ServerHandler serverHandler;
+    private boolean terminate;
+
+    private View currentView;
+    private View nextView;
+
+    public static void main(String[] args){
+        Client client = new Client();
+        client.run();
+    }
+
+    @Override
+    public void run(){
+        //Prendo l'ip dall'utente o lo inserisco io
+        String ip = "xxxxxx";
+
+        Socket server;
+        try{
+            server = new Socket(ip, Server.SOCKET_PORT); //nel server bisogna specificare la porta per comunicare
+        }catch(IOException e){
+            System.out.println("server unreachable");
+            return;
+        }
+        //dopo aver stabilito la connessione con il server, il client delega la connessione all'Handler
+        serverHandler = new ServerHandler(server, this);
+        serverHandlerThread.start();
+
+        nextView = new ChooseInitialPositionView();
+        runViewStateMachine();
+
+        serverHandler.stop();
+    }
+
+    public ServerHandler getServerHandler(){return serverHandler;}
+
+    private void runViewStateMachine(){
+        boolean stop;
+
+        synchronized (this){
+            stop = terminate;
+            currentView = nextView;
+            nextView = null;
+        }
+        while(stop!=false){
+            if(currentView==null){
+                currentView = new WaitingView();  //questa view viene mostrata quando nessun altra view è disponibile
+                                                  //ovvero quando stanno giocando il turno gli altri
+            }
+            currentView.setOwner(this);
+            currentView.run();
+
+            synchronized (this){
+                stop = terminate;
+                currentView = nextView;
+                nextView = null;
+            }
+        }
+    }
+
+
+    public synchronized void transitionView(View nextView){
+        this.nextView = nextView;
+        currentView.setStopInteraction();
+    }
+
+    public synchronized void setTrueTerminate(){
+        if(!terminate){
+            terminate = true;
+            currentView.setStopInteraction();
+        }
+    }
+
+
 }
--- MyShelfie/src/main/java/it/polimi/ingsw/Client/ServerHandler.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Client/ServerHandler.java
@@ -1,4 +1,50 @@
 package it.polimi.ingsw.Client;
 
-public class ServerHandler {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ServerHandler implements Runnable{
+    private Socket server;
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+    private Client owner;
+    private AtomicBoolean shouldStop = new AtomicBoolean(false);
+
+    ServerHandler(Socket server, Client owner){
+        this.server = server;
+        this.owner = owner;
+    }
+
+    //questa run fa andare l'event loop che servirà per ricevere i pacchetti in arrivo dal server
+    @Override
+    public void run(){
+        try{
+            output = new ObjectOutputStream(server.getOutputStream());
+            input = new ObjectInputStream(server.getInputStream());
+        }catch(IOException e){
+            System.out.println("can't open the connection to: "+server.getInetAddress());
+            owner.setTrueTerminate();
+            return;
+        }
+
+        try{
+            handleClientConnection();
+        }catch(IOException e){
+            System.out.println("server "+ server.getInetAddress()+" connection dropped");
+        }
+
+        try{
+            server.close();
+        }catch (IOException e){
+            owner.setTrueTerminate();
+        }
+
+    }
+
+
+    //event loop che riceve i messaggi dal server e li processa
+    //continua...
 }
