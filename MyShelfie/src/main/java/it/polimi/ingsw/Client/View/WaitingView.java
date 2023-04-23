7c9fb1740d1866bf42616c53b6b7e78e51ab7064
matteo-lussana <matteo.lussana@mail.polimi.it>
2023-04-23 11:00:34 +0200

    WaitingView

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/Client/View/WaitingView.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.Client.View;
+
+public class WaitingView extends View{
+
+    @Override
+    public void run()
+    {
+        synchronized (this) {
+            try {
+                this.wait(100);
+            } catch (InterruptedException e) {}
+
+            while (!getStopInteraction()) {
+                String wait = " Please wait...";
+                System.out.print(wait);
+
+                try {
+                    this.wait(500);
+                } catch (InterruptedException e) {}
+
+                for (int i=0; i<wait.length(); i++)
+                    System.out.print("\010");
+
+                }
+            }
+        }
+    }
+
+}
