c8fb021b806060362e7672bcad9fe2cf57cee157
Andrea Giacalone <andrea1.giacalone@mail.polimi.it>
2023-04-26 13:46:01 +0200

    Modified process method in LoginNicknameRequest for linking with controller

--- MyShelfie/src/main/java/it/polimi/ingsw/Server/Messages/LoginNicknameRequest.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Server/Messages/LoginNicknameRequest.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.Server.Messages;
 
 import it.polimi.ingsw.Server.ClientHandler;
+import it.polimi.ingsw.Server.Model.MyShelfie;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,35 +12,34 @@ public class LoginNicknameRequest extends C2SMessage{
 
     public LoginNicknameRequest(String insertedNickname){ this.insertedNickname = insertedNickname; }
 
-    @Override
-    public void processMessage(ClientHandler clientHandler) {
-
-    }
 
     public String getInsertedNickname() {
         return insertedNickname;
     }
 
-    /*@Override
+    @Override
     public void processMessage(ClientHandler clientHandler) {
 
-        clientHandler.getController().checkNickname(insertedNickname);
+        MyShelfie controller = clientHandler.getController();
         S2CMessage loginNicknameAnswer;
 
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException ex) {
-        }
-        if(clientHandler.getController().checkNickname() == true){
-            if(clientHandler.getController().isFirstConnected(insertedNickname) == true ){
+
+
+        if(controller.checkNickname(insertedNickname) == true){
+            controller.addPlayer(insertedNickname);
+            if(controller.isFirstConnected(insertedNickname) == true ){
                 loginNicknameAnswer = new LoginNicknameAnswer(this, LoginNicknameAnswer.Status.FIRST_ACCEPTED);
+
+
             }else{
                 loginNicknameAnswer = new LoginNicknameAnswer(this, LoginNicknameAnswer.Status.ACCEPTED);
             }
+
+
         }else{
             loginNicknameAnswer = new LoginNicknameAnswer(this, LoginNicknameAnswer.Status.INVALID);
         }
 
         clientHandler.sendMessageToClient(loginNicknameAnswer);
-    }*/
+    }
 }
