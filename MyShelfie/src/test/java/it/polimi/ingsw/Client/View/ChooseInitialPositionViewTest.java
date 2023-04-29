39098f658fc11e18e02b3c116f0c6fd5228cd3e3
Irene_Lo_Presti <irene.lopresti@mail.polimi.it>
2023-04-29 16:16:21 +0200

    ChooseInitialPositionViewTest

--- MyShelfie/src/test/java/it/polimi/ingsw/Client/View/ChooseInitialPositionViewTest.java
+++ MyShelfie/src/test/java/it/polimi/ingsw/Client/View/ChooseInitialPositionViewTest.java
@@ -1,12 +1,9 @@
 package it.polimi.ingsw.Client.View;
 
+import it.polimi.ingsw.Server.Messages.YourTurnMsg;
 import it.polimi.ingsw.Server.Model.Board;
-import it.polimi.ingsw.Server.Model.Player;
 import it.polimi.ingsw.Server.Model.ReadFileByLines;
 import it.polimi.ingsw.Server.Model.Tile;
-import it.polimi.ingsw.Server.Messages.BoardMsg;
-import it.polimi.ingsw.Server.Messages.MaxTilesPickableMsg;
-import it.polimi.ingsw.Server.Messages.PlayerNicknameMsg;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,9 +11,7 @@ import org.junit.jupiter.api.Test;
 public class ChooseInitialPositionViewTest {
    public ChooseInitialPositionView chooseInitialPositionView;
 
-   public BoardMsg boardMsg;
-   public PlayerNicknameMsg playerNicknameMsg;
-   public MaxTilesPickableMsg maxTilesPickableMsg;
+   public YourTurnMsg yourTurnMsg;
 
     ReadFileByLines reader;
 
@@ -45,11 +40,9 @@ public class ChooseInitialPositionViewTest {
         //set the grid in board with constructor from file
         board.initFromMatrix(grid);
 
-        boardMsg = new BoardMsg(board);
-        playerNicknameMsg = new PlayerNicknameMsg(new Player("player1"));
-        maxTilesPickableMsg = new MaxTilesPickableMsg(3);
+        yourTurnMsg = new YourTurnMsg("player1", 3, Board.getBoardGrid());
 
-        chooseInitialPositionView = new ChooseInitialPositionView(playerNicknameMsg, maxTilesPickableMsg, boardMsg);
+        chooseInitialPositionView = new ChooseInitialPositionView(yourTurnMsg);
 
     }
 
