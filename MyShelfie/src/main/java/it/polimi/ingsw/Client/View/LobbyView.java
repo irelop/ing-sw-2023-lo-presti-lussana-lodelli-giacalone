c9df5796489376d59936bbfa1f8ba2392f9c8d05
Andrea Giacalone <andrea1.giacalone@mail.polimi.it>
2023-05-01 20:19:41 +0200

    Fixing many things in Lobby e Login view classes

--- MyShelfie/src/main/java/it/polimi/ingsw/Client/View/LobbyView.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Client/View/LobbyView.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.Client.View;
 
 import java.util.ArrayList;
+import java.util.Formatter;
 
 public class LobbyView extends View implements ObserverView {
 
@@ -21,12 +22,27 @@ public class LobbyView extends View implements ObserverView {
     }
 
     public void showPlayerTable(){
+        Formatter fmt = new Formatter();
+
+        fmt.format("%s %15s %15s %s\n","╔","","","╗");
+        fmt.format("%15s %15s\n","NICKNAME","ID");
+        fmt.format("  ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬  \n","","");
+        for(int i = 0; i<lobbyPlayers.size();i++){
+            fmt.format("%15s %15s\n",lobbyPlayers.get(i),(i+1));
+        }
+        fmt.format("%s %15s %15s %s\n","╚","","","╝");
+        System.out.println(fmt);
+
+
+
+        /* Alternative option
         for(int i = 0; i< lobbyPlayers.size();i++) {
-            System.out.format("♦-----------------♦------♦%n");
-            System.out.format("|     " + lobbyPlayers.get(i) + "     |     " + (i+1)+ "     |%n");
-            System.out.format("♦-----------------♦------♦%n");
+            System.out.printf("╔▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬╗%n");
+            System.out.printf("║    %-6s - ID: %-11d║%n",lobbyPlayers.get(i),(i+1));
+            System.out.printf("╚▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬╝%n");
         }
         System.out.println("Players currently waiting:" + lobbyPlayers.size());
+         */
     }
 
     @Override
