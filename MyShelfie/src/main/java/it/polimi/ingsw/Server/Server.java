3367fbfe2fcc782cfd92f88b4978d8427ab38233
Irene_Lo_Presti <irene.lopresti@mail.polimi.it>
2023-05-24 15:11:38 +0200

    add manageServerRMI() in Server: this method manages the connection of an RMI client authors: Lo Presti, Giacalone

--- MyShelfie/src/main/java/it/polimi/ingsw/Server/Server.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Server/Server.java
@@ -5,9 +5,9 @@ import it.polimi.ingsw.Server.Model.MyShelfie;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.ArrayList;
-
-import static it.polimi.ingsw.Server.Model.MyShelfie.*;
 
 
 /**
@@ -20,6 +20,10 @@ public class Server {
     //private static MyShelfie game;
     private static ArrayList<MyShelfie> games;
     private static int currentGame;
+    private static int numRMIClients;
+    private static Object lock;
+    private static RemoteInterface serverInterface;
+    private static Registry registry;
 
 
     /**
@@ -27,9 +31,11 @@ public class Server {
      *           new possible connections with the clients.
      */
     public Server(int serverPort){
-        this.serverPort = 9999;
-        this.currentGame = -1;
-        this.games = new ArrayList<>();
+        serverPort = 9999;
+        currentGame = -1;
+        games = new ArrayList<>();
+        lock = new Object();
+        numRMIClients = 0;
     }
 
     /**
@@ -40,10 +46,10 @@ public class Server {
     public static void main(String[] args) {
         Server server = new Server(serverPort);
         System.out.println("Server is open: listening for new clients...");
-        //qui metteremo due scelte a seconda che venga selezionata la tecnologia di comunicazione RMI o socket
-        manageServerSocket();
-        //else manageServerRMI();
-
+        Thread socketServer = new Thread(()->manageServerSocket());
+        Thread RMIServer = new Thread(()->manageServerRMI());
+        socketServer.start();
+        RMIServer.start();
     }
 
 
@@ -56,7 +62,7 @@ public class Server {
         try{
             socket = new ServerSocket(serverPort);
         }catch (IOException ex){
-            System.out.println("Failed to open server socket");
+            System.out.println("Failed to open socket server");
             System.exit(1);
             return;
         }
@@ -65,17 +71,67 @@ public class Server {
             try{
                 Socket client =socket.accept();
                 // gestione partite multiple
-                if(currentGame == -1 || games.get(currentGame).getAllPlayersReady()){
-                    MyShelfie game = new MyShelfie();
-                    games.add(game);
-                    currentGame++;
-                }
-                ClientHandler clientHandler = new SocketClientHandler(client, games.get(currentGame));
+                synchronized (lock){
+                    if (currentGame == -1 || games.get(currentGame).getAllPlayersReady()) {
+                        MyShelfie game = new MyShelfie();
+                        games.add(game);
+                        currentGame++;
+                    }
+                    ClientHandler clientHandler = new SocketClientHandler(client, games.get(currentGame));
 
-                Thread clientHandlerThread = new Thread(clientHandler,"server_"+client.getInetAddress());
-                clientHandlerThread.start();
+                    Thread clientHandlerThread = new Thread(clientHandler, "server_" + client.getInetAddress());
+                    clientHandlerThread.start();
+                }
             }catch (IOException ex){
                 System.out.println("Connection with server dropped");
+            }
+        }
+    }
+
+    //perché è tutto static?
+    public static void manageServerRMI(){
+        try{
+            serverInterface = new RMIAdapter();
+            registry = LocateRegistry.createRegistry(1099);
+            registry.rebind("server", serverInterface);
+        }catch(Exception e){
+            System.out.println("Failed to open RMI server");
+            System.exit(1);
+            return;
+        }
+
+        while(true){
+
+            //checking if a new client is connected
+            int clientsConnected = numRMIClients;
+            do{
+                try{
+                    clientsConnected = serverInterface.getNumClients();
+                    Thread.sleep(50); //siamo sicuri?
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }while(clientsConnected<=numRMIClients);
+
+            //serve questo if? c'è il do while
+            //o proviamo a togliere il do while? tanto ci sono sleep e while(true)
+            try{
+                if (serverInterface.getNumClients() > numRMIClients) {
+                    synchronized (lock) {
+                        if (currentGame == -1 || games.get(currentGame).getAllPlayersReady()) {
+                            MyShelfie game = new MyShelfie();
+                            games.add(game);
+                            currentGame++;
+                            serverInterface.setController(game);
+                        } else
+                            serverInterface.setController(games.get(currentGame));
+                        numRMIClients++;
+                        //server.setMapClientsToController(games.get(currentGame));
+
+                    }
+                }
+            }catch(Exception e){
+                System.out.println("Problems connecting new RMI client");
             }
         }
     }
