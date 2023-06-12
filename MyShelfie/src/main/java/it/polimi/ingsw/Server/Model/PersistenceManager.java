b44b81b98ba7d3001cb7c38da5031434da5d25d4
Irene_Lo_Presti <irene.lopresti@mail.polimi.it>
2023-06-12 21:10:03 +0200

    javadoc of PersistenceManager

--- MyShelfie/src/main/java/it/polimi/ingsw/Server/Model/PersistenceManager.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Server/Model/PersistenceManager.java
@@ -1,14 +1,15 @@
 package it.polimi.ingsw.Server.Model;
 
-import it.polimi.ingsw.Server.Model.ReadFileByLines;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static java.lang.Integer.valueOf;
+/**
+ * Class to manage the files for the FA persistence
+ * @author Irene Lo Presti
+ */
 
 public class PersistenceManager {
     private String path;
@@ -22,6 +23,10 @@ public class PersistenceManager {
     }
 
 
+    /**
+     * Given the name, this method creates a new file for a new game and adds it to the array list
+     * @param name of the new file: game_indexOfTheGame.txt
+     */
     public void addNewGameFile(String name){
         File file = new File(path+name);
         try {
@@ -31,13 +36,19 @@ public class PersistenceManager {
             System.out.println("Problems with file for server persistence creation.");
         }
     }
-    public void addNewPlayerFile(String name, int controllerIdx){
+
+    /**
+     * Given the name, this method creates a new file for a new player and adds it to the array list
+     * @param name of the new file: playerNickname.txt
+     * @param gameIdx index of the player's game to be written in the file
+     */
+    public void addNewPlayerFile(String name, int gameIdx){
         File file = new File(path+name);
         try {
             if(file.createNewFile()) {
                 FileWriter fw = new FileWriter(file);
                 BufferedWriter bw = new BufferedWriter(fw);
-                bw.write(controllerIdx + "\n");
+                bw.write(gameIdx + "\n"); //write the index of the game in the player's file
                 bw.flush();
                 bw.close();
                 this.playersFiles.add(file);
@@ -48,18 +59,36 @@ public class PersistenceManager {
 
     }
 
+    /**
+     * This method deletes the file of the player whose nickname is passed as a parameter and removes it from
+     * the array list
+     * @param nickname of the player
+     */
     public void deletePlayerFile(String nickname){
         File file = new File(path + nickname + ".txt");
         int index = playersFiles.indexOf(file);
         boolean success = playersFiles.get(index).delete();
         if(success)
             playersFiles.remove(index);
+        else System.out.println("Problems deleting "+nickname+"'s persistence file");
     }
 
+    /**
+     * Getter method
+     * @param index of the game
+     * @return the file of the game at position 'index'
+     */
     public File getGameFile(int index){
         return gameFiles.get(index);
     }
 
+    /**
+     * This method deletes the file of the game with index 'index' and removes it from
+     * the array list
+     * @param index of the game
+     * @param removeFromList if true the method remove the game from the array list, otherwise it sets it null
+     *                       to preserve the enumeration
+     */
     public void deleteGameFile(int index, boolean removeFromList){
         boolean success = gameFiles.get(index).delete();
         if(success){
@@ -70,8 +99,12 @@ public class PersistenceManager {
 
     }
 
+    /**
+     * FA: persistence. This method finds all the persistence file, and it saves them into the arraylist
+     * @return the number of the old games
+     */
     public int reset(){
-        int gameIdx = 0, gamesNum = 0;//serve??
+        int gameIdx = 0, gamesNum = 0;
 
         while(true){
             String pathFile = path + "game_" + gameIdx + ".txt";
