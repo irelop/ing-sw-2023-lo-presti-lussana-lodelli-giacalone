3cabce65eb1b98f2b68840be770d9a712bc5588a
Andrea Giacalone <andrea1.giacalone@mail.polimi.it>
2023-06-21 09:32:39 +0200

    add javadoc -lastplayerconnectedview

--- MyShelfie/src/main/java/it/polimi/ingsw/Client/view/CLI/LastPlayerConnectedView.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Client/view/CLI/LastPlayerConnectedView.java
@@ -3,11 +3,23 @@ package it.polimi.ingsw.Client.view.CLI;
 import it.polimi.ingsw.Server.Messages.FinishGameRequest;
 import it.polimi.ingsw.Server.Messages.LastOneConnectedMsg;
 
+/**
+ * LastPlayerConnectedView class: this view is shown when there is only one player left connected to the game.
+ * A countdown will start and until its end, all the other players, who has disconnected before, have the possibility
+ * to rejoin the game, otherwise the player waiting in this view will be declared as the winner.
+ *
+ * @authors Irene Lo Presti, Andrea Giacalone
+ */
 public class LastPlayerConnectedView extends View{
 
     private LastOneConnectedMsg msg;
-
     private boolean goOn = true;
+
+    /**
+     * CONSTRUCTOR: a new LastPlayerConnectedView is built wrapping a message containing the infos about the last player
+     * connected to the game.
+     * @param msg : message wrapping info of the last player connected.
+     */
     public LastPlayerConnectedView(LastOneConnectedMsg msg){
         this.msg = msg;
     }
@@ -18,6 +30,7 @@ public class LastPlayerConnectedView extends View{
                 "At least one of them has to reconnect to this game in 30 seconds or you will be the winner.\n" +
                 "\nCOUNTDOWN:\n");
         int i;
+        //starting the countdown
         for(i=30; i>=0 && goOn; i--){
             System.out.println(i+" seconds");
             try {
@@ -26,19 +39,25 @@ public class LastPlayerConnectedView extends View{
                 throw new RuntimeException(e);
             }
         }
-        //if(i==0) {
+        //the countdown wasn't notified by any event of reconnection: the last player connected wins the game
         if(goOn) {
             System.out.println("Congratulation " + msg.nickname + "! You are the winner!");
             System.out.println("See you soon " + msg.nickname + "!");
+
+            //building the message to finish the game
             FinishGameRequest finishGameRequest = new FinishGameRequest();
             getOwner().getServerHandler().sendMessageToServer(finishGameRequest);
 
-            getOwner().setTrueTerminate();
+            getOwner().setTrueTerminate();  //stopping the view state machine
         }
 
-        // }
     }
 
+    /**
+     * OVERVIEW: this method allows to notify the player in countdown mode about the reconnection of at least one other
+     * player previously disconnected to the game.
+     * @param nickname: the nickname of the player now reconnected to the game
+     */
     public void notifyView(String nickname) {
         this.goOn = false;
         System.out.println(nickname+" reconnected, the game continues! Good luck!");
