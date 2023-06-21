37e4d10ac73283e95e6d89cc34e9280f8c6135be
riccardo lodelli <riccardo.lodelli@mail.polimi.it>
2023-06-21 10:24:33 +0200

    add gui if in methods (ReconnectionAnswer)

--- MyShelfie/src/main/java/it/polimi/ingsw/Server/Messages/ReconnectionAnswer.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Server/Messages/ReconnectionAnswer.java
@@ -20,18 +20,26 @@ public class ReconnectionAnswer extends S2CMessage{
 
     @Override
     public void processMessage(ServerHandler serverHandler) {
-        serverHandler.getOwner().setNickname(nickname);     //re-setting the nickname
-        LoginView loginView = (LoginView) serverHandler.getOwner().getCurrentView();
-        loginView.setReconnectionAnswer(this);
-        serverHandler.getOwner().getCurrentView().notifyView();
+        if(serverHandler.getOwner().gui) {
+            serverHandler.getOwner().getStageManager().getController().receiveAnswer(this);
+        } else {
+            serverHandler.getOwner().setNickname(nickname);     //re-setting the nickname
+            LoginView loginView = (LoginView) serverHandler.getOwner().getCurrentView();
+            loginView.setReconnectionAnswer(this);
+            serverHandler.getOwner().getCurrentView().notifyView();
+        }
     }
 
     @Override
     public void processMessage(RemoteInterface server, RemoteInterface client) {
         try {
-            client.getOwner().setNickname(nickname);     //re-setting the nickname
-            LoginView loginView = (LoginView) client.getOwner().getCurrentView();
-            loginView.setReconnectionAnswer(this);
+            if(client.getOwner().gui) {
+                client.getOwner().getStageManager().getController().receiveAnswer(this);
+            } else {
+                client.getOwner().setNickname(nickname);     //re-setting the nickname
+                LoginView loginView = (LoginView) client.getOwner().getCurrentView();
+                loginView.setReconnectionAnswer(this);
+            }
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         }
